78b46b5be8275ac7b66a7968525d29a59579248f
nukambe <chappellwesley@gmail.com>
2024-10-16 19:40:08 -0400

    Add more skills

--- src/models/Skill.model.ts
+++ src/models/Skill.model.ts
@@ -1,25 +1,28 @@
 export enum tag {
-    frontend = "Frontend",
-    backend = "Backend",
-    database = "Database",
-    devops = "DevOps",
-    mobile = "Mobile",
-    desktop = "Desktop",
-    cloud = "Cloud",
-    testing = "Testing",
-    security = "Security",
-    OOP = "OOP",
-    ORM = "ORM",
+  frontend = 'Frontend',
+  backend = 'Backend',
+  database = 'Database',
+  devops = 'DevOps',
+  mobile = 'Mobile',
+  desktop = 'Desktop',
+  cloud = 'Cloud',
+  testing = 'Testing',
+  security = 'Security',
+  OOP = 'OOP',
+  ORM = 'ORM',
+  EVENT = 'Event Streaming',
+  GAMING = 'Gaming',
+  VC = 'Version Control',
 }
 
 export class Skill {
-    id: number;
-    name: string = "NAME";
-    description: string = "DESCRIPTION";
-    image: string = "IMAGE";
-    tags: tag[] = [];
+  id: number;
+  name: string = 'NAME';
+  description: string = 'DESCRIPTION';
+  image: string = 'IMAGE';
+  tags: tag[] = [];
 
-    constructor(id: number) {
-        this.id = id;
-    }
+  constructor(id: number) {
+    this.id = id;
+  }
 }
